d992e603551c7f67cf28c4bca76b0f4b308abaaf
AbelRR <reyesregalado@gmail.com>
2018-09-09 13:34:43 -0700

    Fixed Linting Issues with stockDataGenerator.js

--- stockDataGenerator.js
+++ stockDataGenerator.js
@@ -1,90 +1,91 @@
-var faker = require('faker');
+const faker = require('faker');
 
 function getRandomValue(min, max) {
-    return Math.random() * (max - min) + min;
+  return Math.random() * (max - min) + min;
 }
 
-function percentageChange(valOne, valTwo){
-    return(((valTwo - valOne)/valOne)*100);
+function percentageChange(valOne, valTwo) {
+  return (((valTwo - valOne) / valOne) * 100);
 }
 
-function stocksPurchased(startingVal, weekAverage){
-        //AVERAGE PRICE
-    if (percentageChange(startingVal, weekAverage) < 4 && percentageChange(startingVal, weekAverage) > -4){
-        return Math.floor(getRandomValue(850, getRandomValue(1250, 2300)))
-        //HIGH PRICE
-    } else if(percentageChange(startingVal, weekAverage) > 4){
-        return Math.floor(getRandomValue(70, getRandomValue(150, 450)))
-        //LOW PRICE
-    } else if(percentageChange(startingVal, weekAverage) < -4){
-        return Math.floor(getRandomValue(500, getRandomValue(750, 1300)))
-    }
+function stocksPurchased(startingVal, weekAverage) {
+  // AVERAGE PRICE
+  let returnVal;
+  if (percentageChange(startingVal, weekAverage) < 4
+        && percentageChange(startingVal, weekAverage) > -4) {
+    returnVal = Math.floor(getRandomValue(850, getRandomValue(1250, 2300)));
+    // HIGH PRICE
+  } if (percentageChange(startingVal, weekAverage) > 4) {
+    returnVal = Math.floor(getRandomValue(70, getRandomValue(150, 450)));
+    // LOW PRICE
+  } if (percentageChange(startingVal, weekAverage) < -4) {
+    returnVal = Math.floor(getRandomValue(500, getRandomValue(750, 1300)));
+  }
+  return returnVal;
 }
 
-var weeklyValues = [];
-var totalStocksPurchased = 0;
-var weekValues = function(startingVal, index) {
+let weeklyValues = [];
+let totalStocksPurchased = 0;
+function weekValues(startingVal, index) {
+  const minusSomePercent = startingVal - (startingVal * getRandomValue(0.03, 0.38));
+  const plusSomePercent = startingVal + (startingVal * getRandomValue(0.03, 0.52));
 
-    var minusSomePercent = startingVal - (startingVal * getRandomValue(.03, .38));
-    var plusSomePercent = startingVal + (startingVal * getRandomValue(.03, .52));
+  const weekHigh = parseFloat(getRandomValue(startingVal, plusSomePercent).toFixed(2));
+  const weekLow = parseFloat(getRandomValue(minusSomePercent, startingVal).toFixed(2));
+  const weekAverage = parseFloat(((weekLow + weekHigh) / 2).toFixed(2));
+  const weekStocksPurchased = stocksPurchased(startingVal, weekAverage);
 
-    var weekHigh = parseFloat(getRandomValue(startingVal, plusSomePercent).toFixed(2));
-    var weekLow = parseFloat(getRandomValue(minusSomePercent, startingVal).toFixed(2));
-    var weekAverage = parseFloat(((weekLow + weekHigh) / 2).toFixed(2));
-    var weekStocksPurchased = stocksPurchased(startingVal, weekAverage);
+  const weekObj = {
+    weekIndex: index,
+    weekHigh,
+    weekLow,
+    weekAverage,
+    weekStocksPurchased,
+  };
 
-    var weekObj = {
-        weekIndex: index,
-        weekHigh,
-        weekLow,
-        weekAverage,
-        weekStocksPurchased
-    }
-
-    weeklyValues.push(weekObj.weekAverage);
-    totalStocksPurchased += weekStocksPurchased;
-    return weekObj;
+  weeklyValues.push(weekObj.weekAverage);
+  totalStocksPurchased += weekStocksPurchased;
+  return weekObj;
 }
 
-
-
-var xWeeks = function(numOfWeeks, avgCompanyValue) {
-    var results = [];
-    for (var i = 0; i < numOfWeeks; i++ ) {
-        oneWeek = weekValues(avgCompanyValue, i+1);
-        results.push(oneWeek);
-        // results.push(JSON.stringify(oneWeek));
-    }
-    return results;
+function xWeeks(numOfWeeks, avgCompanyValue) {
+  const results = [];
+  for (let i = 0; i < numOfWeeks; i += 1) {
+    const oneWeek = weekValues(avgCompanyValue, i + 1);
+    results.push(oneWeek);
+    // results.push(JSON.stringify(oneWeek));
+  }
+  return results;
 }
 
-var testCompany = function(x) {
-    var results = [];
+function testCompany(x) {
+  const results = [];
 
-    for (var i = 0; i < x; i++ ) {
-        var companyName = faker.company.companyName();
-        var startingVal = getRandomValue(9, getRandomValue(70, 780));
-        var companyObj = {
-            company: companyName,
-            weeks: xWeeks(52, startingVal), 
-            yearly: {
-                stocksPurchasedYear: totalStocksPurchased,
-                yearHighest: Math.max.apply(null, weeklyValues),
-                yearLowest: Math.min.apply(null, weeklyValues),
-                yearAverage: parseFloat(((Math.max.apply(null, weeklyValues) + Math.min.apply(null, weeklyValues)) / 2).toFixed(2))    
-            }
-            
-        }
-        results.push(companyObj); //
-        // results.push(JSON.stringify(companyObj)); //
-        weeklyValues = []
-        totalStocksPurchased = 0
-    }
+  for (let i = 0; i < x; i += 1) {
+    const companyName = faker.company.companyName();
+    const startingVal = getRandomValue(9, getRandomValue(70, 780));
+    const companyObj = {
+      company: companyName,
+      weeks: xWeeks(52, startingVal),
+      yearly: {
+        stocksPurchasedYear: totalStocksPurchased,
+        yearHighest: Math.max.apply(null, weeklyValues),
+        yearLowest: Math.min.apply(null, weeklyValues),
+        yearAverage: parseFloat(((Math.max.apply(null, weeklyValues)
+                        + Math.min.apply(null, weeklyValues)) / 2).toFixed(2)),
+      },
 
-    return results;
+    };
+    results.push(companyObj); //
+    // results.push(JSON.stringify(companyObj)); //
+    weeklyValues = [];
+    totalStocksPurchased = 0;
+  }
+
+  return results;
 }
 
 // var output = (testCompany(99));
-var output = JSON.stringify(testCompany(99));
+const output = JSON.stringify(testCompany(99));
 
-console.log(output)
+console.log(output);
