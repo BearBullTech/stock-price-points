0ef7bdfee9d55984232b083980103b6b2d68c826
AbelRR <reyesregalado@gmail.com>
2018-09-16 12:45:15 -0700

    misc

--- client/CurrentPrice/CurrentPrice.jsx
+++ client/CurrentPrice/CurrentPrice.jsx
@@ -3,20 +3,18 @@ import PropTypes from 'prop-types';
 import PriceBall from './PriceBall/PriceBall.jsx';
 import VerticalLine from './VerticalLine/VerticalLine.jsx';
 
-const CurrentPrice = ({ priceOnTheLine, upDownColor }) => {
-  return (
-    <g id="priceOnTheLine">
-      <VerticalLine
-        upDownColor={upDownColor}
-        priceOnTheLine={priceOnTheLine}
-      />
-      <PriceBall
-        upDownColor={upDownColor}
-        priceOnTheLine={priceOnTheLine}
-      />
-    </g>
-  );
-};
+const CurrentPrice = ({ priceOnTheLine, upDownColor }) => (
+  <g id="priceOnTheLine">
+    <VerticalLine
+      upDownColor={upDownColor}
+      priceOnTheLine={priceOnTheLine}
+    />
+    <PriceBall
+      upDownColor={upDownColor}
+      priceOnTheLine={priceOnTheLine}
+    />
+  </g>
+);
 
 // propTypes
 CurrentPrice.propTypes = {
--- client/CurrentPrice/VerticalLine/VerticalLine.jsx
+++ client/CurrentPrice/VerticalLine/VerticalLine.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const VerticalLine = ({ priceOnTheLine, upDownColor }) => {
-  return (
-    <line
-      y1="-35"
-      y2="110"
-      x1={`${priceOnTheLine}`}
-      x2={`${priceOnTheLine}`}
-      fill={upDownColor}
-      stroke={upDownColor}
-    />
-  );
-};
+const VerticalLine = ({ priceOnTheLine, upDownColor }) => (
+  <line
+    y1="-35"
+    y2="110"
+    x1={`${priceOnTheLine}`}
+    x2={`${priceOnTheLine}`}
+    fill={upDownColor}
+    stroke={upDownColor}
+  />
+);
 
 // propTypes
 VerticalLine.propTypes = {
